2811ffc185d23b0f7e0abc465215c81c33090320
KurokiEiyu <114289741+KurokiEiyu@users.noreply.github.com>
2022-11-07 13:20:12 +0700

    Add files via upload

---
+++ PBO1Sesi6/InterfaceKendaraan.java
@@ -0,0 +1,9 @@
+package PBO1Sesi6;
+public interface InterfaceKendaraan {
+    
+    String merk="";
+    int tahun=0;
+    String warna="";
+    
+    public void infoKendaraan();
+}
---
+++ PBO1Sesi6/Kendaraan.java
@@ -0,0 +1,13 @@
+package PBO1Sesi6;
+public abstract class Kendaraan {
+    
+    String merk;
+    private int tahun;
+    protected String warna;
+    
+    abstract void infoKendaraan();
+    
+    void infoKendaraanAbstract(){
+        
+    }
+}
